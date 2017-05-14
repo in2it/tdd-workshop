0f79b526555df1a53348a1d603a993ecfe295478
Michelangelo van Dam <michelangelo@in2it.be>
2017-05-14 15:30:51 +0200

    Creating a test skeleton
    This skeleton will allow us to focus on the project scope

---
+++ tests/Service/TaskServiceTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Test\Service;
+
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TaskServiceTest
+ *
+ * @package App\Test\Service
+ * @group Service
+ */
+class TaskServiceTest extends TestCase
+{
+    public function testServiceReturnsListOfTasks()
+    {
+        // List open tasks sorted newest to oldest
+    }
+
+    public function testServiceCanAddNewTask()
+    {
+        // Create a new task (label and description)
+    }
+
+    public function testServiceCanUpdateExistingTask()
+    {
+        // Update an existing task
+    }
+
+    public function testServiceCanMarkTaskAsDone()
+    {
+        // Mark task as done in the overview list
+    }
+
+    public function testServiceCanRemoveTaskMarkedAsDone()
+    {
+        // Remove task marked as done
+    }
+}
